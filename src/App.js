ff902631b8b5a88e836c8eb68699473f32572eae
Pasindu Laksara <142964169+Pasindu-Laksara@users.noreply.github.com>
2024-01-27 16:54:26 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ import Budget from './components/Budget';
 
 
 import { AppProvider } from './context/AppContext';
+import ExpenseList from './components/ExpenseList';
+import Remaining from './components/Remaining';
+import ExpenseItem from './components/ExpenseItem'; 
+import ExpenseTotal from './components/ExpenseTotal';
+import AllocationForm from './components/AllocationForm';
+
 const App = () => {
     return (
         <AppProvider>
@@ -17,26 +23,42 @@ const App = () => {
                     <div className='row mt-3'>
                         {
                             /* Add Budget component here */
+                                            // Budget component
+                    <div className='col-sm'>
+                    <Budget />
+                </div>
                         }        
 
                         {
                             /* Add Remaining component here*/
+                                             //Remaining component
+                    <div className='col-sm'>
+                    <Remaining />
+                </div>
                         }        
 
                         {
                             /* Add ExpenseTotal component here */
+                                             //ExpenseTotal component
+                    <div className='col-sm'>
+                    <ExpenseTotal />
+                </div>
                         }        
                        
                         {
                             /* Add ExpenseList component here */
+                            <ExpenseList />
                         }         
 
                         {
                             /* Add ExpenseItem component here */
+                            <ExpenseItem />
+                            
                         }        
 
                         {
                             /* Add AllocationForm component here under */
+                            <AllocationForm />
                         }        
 
                 </div>
